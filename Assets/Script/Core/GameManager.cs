83c76918c951c9f146929c2c9fc2ff4e825a5591
Astrid Fayyaza Danishara <fayyazaastrid@gmail.com>
2026-05-19 16:45:22 +0700

    Membuat pause menu

--- Assets/Script/Core/GameManager.cs
+++ Assets/Script/Core/GameManager.cs
@@ -3,11 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
+   public GameObject pausePanel;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
     public void StartGame()
     {
         Debug.Log("Start ditekan!");
         SceneManager.LoadScene("Eco_Villo");
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Pause ditekan!");
+        
+        if (!pausePanel.activeSelf)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f; 
+        } else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("Main Menu ditekan!");
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void ExitGame()
